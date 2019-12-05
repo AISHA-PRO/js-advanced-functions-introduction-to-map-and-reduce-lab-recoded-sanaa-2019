87ed575d7fe5992b87c29df342dd44e1359edf62
Aisha Rashed <aisha1rashed@gmail.com>
2019-12-05 21:07:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+const mapToNegativize = function(sourceArray){
+  let arr = sourceArray.map(e=>-e);
+  return arr;
+}
+const mapToNoChange = function(sourceArray){
+   let arr = sourceArray.map(e=>e);
+  return arr;
+}
+const mapToDouble = function(sourceArray){
+   let arr = sourceArray.map(e=>2*e);
+  return arr;
+}
+const mapToSquare = function(sourceArray){
+   let arr = sourceArray.map(e=>4*e);
+  return arr;
+}
